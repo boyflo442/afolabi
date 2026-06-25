68edf8a72404e63a869414f4621614b3ff14a526
Adedoye Afolabi <adedoyeafolabi4@gmail.com>
2026-06-25 23:02:47 +0000

    Update query `My first shared query`

--- 3222858-My first shared query.sql
+++ 3222858-My first shared query.sql
@@ -9,7 +9,11 @@ SELECT * FROM student;
 
 INSERT INTO student VALUES(1, 'Jack', 'Biology');
 INSERT INTO student VALUES(2, 'Kate', 'Sociology');
-INSERT INTO student VALUES(3, 'Claire', 'Catering');
+INSERT INTO student(student_id, name) VALUES(3, 'Claire');
 INSERT INTO student VALUES(4, 'John', 'Biology');
 INSERT INTO student VALUES(5, 'Mike', 'Computer Science');
-INSERT INTO student VALUES(6, 'Afolabi', 'Computer Science');
+INSERT INTO student VALUES(6, 'Afolabi', 'Computer Science')
+
+UPDATE student
+SET major ='software engineering'
+WHERE major = 'null';
